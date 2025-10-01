47c210ea5b7df0efb9450debd9a20b606c5c1c21
ahiu2001lc <ahiu2001lc@gmail.com>
2025-10-01 09:26:54 +0700

    feature: add hello/endpoint

---
+++ src/main/java/com/example/demo_git/controller/PublicApiController.java
@@ -0,0 +1,15 @@
+package com.example.demo_git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class PublicApiController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello from feature/hello-api!";
+    }
+}
